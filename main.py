24da09bab55472272e645ad91b8760cefbfbc8bf
Mohan Krishna Gupta <61947133+mohan-gupta@users.noreply.github.com>
2022-09-11 21:10:36 +0530

    updated notebook, and streamlit UI

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
+import base64
 
 model = load_model('Model/model.h5')
 class_dict = np.load("artifacts/class_names.npy")
@@ -10,41 +11,53 @@ class_dict = np.load("artifacts/class_names.npy")
 def predict(image):
     IMG_SIZE = (1, 224, 224, 3)
 
-    img_arr = np.array(image)
-    img_arr = np.resize(img_arr, (IMG_SIZE[1:]))
-    img_arr = np.reshape(img_arr, IMG_SIZE)
+    img = image.resize(IMG_SIZE[1:-1])
+    img_arr = np.array(img)
+    img_arr = img_arr.reshape(IMG_SIZE)
 
-    print(img_arr.shape)
-    pred = model.predict(img_arr)
-
+    pred_proba = model.predict(img_arr)
+    pred = np.argmax(pred_proba)
     return pred
 
-if __name__ == '__main__':
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-        f"""
-             <style>
-             .stApp {{
-                 background: url("https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
-                 background-size: cover
-             }}
-             </style>
-             """,
-        unsafe_allow_html=True
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
     )
+
+contnt = "<p>Herbal medicines are preferred in both developing and developed countries as an alternative to " \
+         "synthetic drugs mainly because of no side effects. Recognition of these plants by human sight will be " \
+         "tedious, time-consuming, and inaccurate.</p> " \
+         "<p>Applications of image processing and computer vision " \
+         "techniques for the identification of the medicinal plants are very crucial as many of them are under " \
+         "extinction as per the IUCN records. Hence, the digitization of useful medicinal plants is crucial " \
+         "for the conservation of biodiversity.</p>"
+
+if __name__ == '__main__':
+    add_bg_from_local("artifacts/Background.jpg")
     new_title = '<p style="font-family:sans-serif; color:White; font-size: 42px;">Medicinal Leaf Classification</p>'
     st.markdown(new_title, unsafe_allow_html=True)
+    st.markdown(contnt, unsafe_allow_html=True)
     uploaded_file = st.file_uploader("Choose a file")
 
     if uploaded_file is not None:
         img = Image.open(uploaded_file)
-        img_arr = np.array(img)
 
         img = img.resize((300, 300))
         st.image(img)
 
         if st.button("Predict"):
-            pred = predict(img_arr)
-            name = class_dict[np.argmax(pred)]
+            pred = predict(img)
+            name = class_dict[pred]
 
             result = '<p style="font-family:sans-serif; color:White; font-size: 16px;">The given image ' \
                         'is '+name+'</p>'
